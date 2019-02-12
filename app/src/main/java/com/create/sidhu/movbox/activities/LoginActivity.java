5f4a0ccd247cbb5ace86d8bb834a7c2378227af2
Nihal Pradeep <nihalpradeep@roninaks.com>
2019-02-12 18:18:19 +0530

    Facebook Login Issue

--- app/src/main/java/com/create/sidhu/movbox/activities/LoginActivity.java
+++ app/src/main/java/com/create/sidhu/movbox/activities/LoginActivity.java
@@ -26,11 +26,13 @@ import com.create.sidhu.movbox.helpers.PermissionsHelper;
 import com.create.sidhu.movbox.helpers.SqlHelper;
 import com.create.sidhu.movbox.helpers.StringHelper;
 import com.create.sidhu.movbox.models.UserModel;
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -252,6 +254,10 @@ public class LoginActivity extends AppCompatActivity implements SqlDelegate {
                 Intent signInIntent = googleSignInClient.getSignInIntent();
                 startActivityForResult(signInIntent, PermissionsHelper.REQUEST_GOOGLE_AUTHENTICATION);
             } else if (type.equals(getString(R.string.fb_signin))) {
+                AccessToken accessToken = AccessToken.getCurrentAccessToken();
+                boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
+                if (isLoggedIn)
+                    LoginManager.getInstance().logOut();
                 loginButton.performClick();
             }
         }catch (Exception e){
